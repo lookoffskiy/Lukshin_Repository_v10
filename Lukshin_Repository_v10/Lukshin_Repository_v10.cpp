c8573b9663476e30388bcddb853b88c39add9668
lkoff <lukshin046@gmail.com>
2024-06-07 10:50:23 +0300

    Edit functions and Main

--- Lukshin_Repository_v10/Lukshin_Repository_v10.cpp
+++ Lukshin_Repository_v10/Lukshin_Repository_v10.cpp
@@ -17,21 +17,29 @@ int OstatDel()
 {
     int Ost;
     Ost = (A + B) % C;
-    return Ost;
+    std::cout << Ost << endl;
 }
 int CelDel()
 {
     int Cel;
     Cel = (A + B) / C;
-    return Cel;
+    std::cout << Cel << endl;
 }
 int main()
 {
     setlocale(LC_ALL, "RU");
 
+    int n;
     std::cout << "1. Введите целые числа A, B и C\n";
     std::cout << "2. Найти остаток от деления суммы целых чисел А и В на число С\n";
     std::cout << "3. Найти целую часть от деления суммы целых чисел А и В на число С\n";
     std::cout << "\n";
     std::cout << "Введите номер пункта, который хотели бы выполнить: ";
+
+    switch (n)
+    {
+    case 1: EnterNumbers();
+    case 2: OstatDel();
+    case 3: CelDel();
+    }
 }
