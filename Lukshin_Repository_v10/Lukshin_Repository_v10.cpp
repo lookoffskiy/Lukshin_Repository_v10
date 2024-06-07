4d1dd20bc8b1c54f4769ce8a385c2106468c8db1
lkoff <lukshin046@gmail.com>
2024-06-07 10:25:18 +0300

    Create void main

--- Lukshin_Repository_v10/Lukshin_Repository_v10.cpp
+++ Lukshin_Repository_v10/Lukshin_Repository_v10.cpp
@@ -1,7 +1,27 @@
 ﻿
 #include <iostream>
 
+using namespace std;
+
+int EnterNumbers()
+{
+
+}
+int OstatDel()
+{
+
+}
+int CelDel()
+{
+
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "RU");
+
+    std::cout << "1. Введите целые числа A, B и C\n";
+    std::cout << "2. Найти остаток от деления суммы целых чисел А и В на число С\n";
+    std::cout << "3. Найти целую часть от деления суммы целых чисел А и В на число С\n";
+    std::cout << "\n";
+    std::cout << "Введите номер пункта, который хотели бы выполнить: ";
 }
